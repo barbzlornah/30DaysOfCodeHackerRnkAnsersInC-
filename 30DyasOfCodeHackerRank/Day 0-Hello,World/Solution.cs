c11e264b992271e021493a5df28dd62983cd9c91
Barbara Masinde <barbzlornah@gmail.com>
2020-03-02 14:48:16 +0300

    Add project files.

---
+++ 30DyasOfCodeHackerRank/Day 0-Hello,World/Solution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+    class Solution
+    {
+        static void Main(String[]args)
+        {
+            //Declare a variable named 'inputVar' to hold input
+            String inputVar;
+
+            //read a full line of input from stdin, and save it to our variable,inputVar
+            inputVar = Console.ReadLine();
+
+            //print the sting 'Hello, World.' to stdout
+            Console.WriteLine("Hello, World.");
+
+            //print the content of the input variable to stdout
+            Console.WriteLine(inputVar);
+        }
+    }
+
