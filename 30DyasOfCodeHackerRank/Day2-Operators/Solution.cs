8636fe0641e764536ed7a113340dbf6b29308b0f
Barbara Masinde <barbzlornah@gmail.com>
2020-06-18 21:03:06 +0300

    New

---
+++ 30DyasOfCodeHackerRank/Day2-Operators/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _30DyasOfCodeHackerRank.Day2_Operators
+{
+    class Solution
+    {
+        static void Main(String[] args)
+        {
+            var mealCost = double.Parse(Console.ReadLine());
+            var tipPercent = int.Parse(Console.ReadLine());
+            var taxPercent = int.Parse(Console.ReadLine());
+
+            var tip = tipPercent * mealCost / 100;
+            var tax = taxPercent * mealCost / 100;
+
+            var totalCost = Math.Round(tip + tax + mealCost);
+            Console.WriteLine(totalCost);
+        }
+    }
+}
---
+++ 30DyasOfCodeHackerRank/Day3-Conquer Conditionals/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _30DyasOfCodeHackerRank.Day3_Conquer_Conditionals
+{
+    class Solution
+    {
+        static void Main(String[]args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            if (n % 2 != 0) Console.WriteLine("Weird");
+            else
+            {
+                if (n < 5) Console.WriteLine("Not Weird");
+                else if (n <= 20) Console.WriteLine("Weird");
+                else Console.WriteLine("Not Weird");
+            }
+        }
+    }
+}
